3b5492f36401b2638f90d438f86753b54089c21b
Elias Alejandro Diaz Cahuas <N00369765@upn.pe>
2023-09-21 23:58:16 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("\tLogaritmo para hallar la potencia de un número");
+Console.WriteLine("\t----------------------------------------------\n");
+Console.Write("Ingrese la base: ");
+float bas = float.Parse(Console.ReadLine());
+Console.Write("Ingrese el exponente: ");
+int exponente = int.Parse(Console.ReadLine());
+double potencia = bas;
+
+if (exponente >= 0)
+{
+    if (exponente == 0)
+    {
+        potencia = 1;
+    }
+    else
+    {
+        for (int i = 1; i < exponente; i++)
+        {
+            potencia *= bas;
+        }
+    }
+}
+else
+{
+    Console.WriteLine("¡Error! Este algoritmo funciona solo con exponentes iguales o mayores a cero");
+    Console.ReadKey();
+    return;
+}
+
+Console.WriteLine("La potencia es: " + potencia);
+Console.ReadKey(); 
